f84b2645fd941d69237a6a063e06ca80263da26f
evelin-mart <evalouis666@gmail.com>
2022-03-28 17:31:16 +0300

    feat: add answers

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let sorted = arr.filter((item) => item !== -1).sort((a, b) => a - b);
+  let index = arr.indexOf(-1);
+  while (index !== -1) {
+    sorted.splice(index, 0, -1);
+    index = arr.indexOf(-1, index + 1);
+  }
+  return sorted;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,46 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--double-next":
+        if (i + 1 <= arr.length - 1) result.push(arr[i + 1]);
+        break;
+      case "--discard-prev":
+        result.pop(result.length - 1);
+        break;
+      case "--discard-next":
+        i++;
+        break;
+      case "--double-prev":
+        if (i - 1 >= 0) result.push(arr[i - 1]);
+        break;
+      default:
+        result.push(arr[i]);
+        break;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
-  transform
+  transform,
 };
